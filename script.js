a92858a6020dc32d87b90ae92e6c5fe1217aafa2
roshanpaudel <paudelroshan93@gmail.com>
2025-04-28 12:36:34 +1000

    slider function form homecreen to appscreen working

--- script.js
+++ script.js
@@ -7,10 +7,20 @@ slider.addEventListener("change", (e) => {
   console.log(value);
   if (value > 80) {
     label.textContent = "";
+    appScreenLoader();
   } else {
     label.textContent = "Slide to unlock";
     e.target.value = 0;
   }
 });
 
-const appScreenLoader = () => {};
+const appScreenLoader = () => {
+  const homescreen = document.getElementsByClassName("homescreen");
+  console.log(homescreen); //returns HTMLCollection
+
+  //selects the first items of HTMLCollection that has same classname
+  homescreen[0].style.display = "none";
+
+  const appScreen = document.getElementsByClassName("appScreen");
+  appScreen[0].style.display = "";
+};
